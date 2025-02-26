1f0e40ad64ee8450cfb139914c92ab988aa1ab4c
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-02-26 23:53:37 +0100

    Add all the quotes

---
+++ quotes.js
@@ -0,0 +1,145 @@
+export const quotes = [{
+  quote: 'Be yourself; everyone else is already take.',
+  type: 'Popular quote'
+},{
+  quote: 'So many books so little time.',
+  type: 'Popular quote'
+},{
+  quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+  type: 'Popular quote'
+},{
+  quote: 'A room without book is like a bed without a soul.',
+  type: 'Popular quote'
+},{
+  quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mnd.",
+  type: 'Popular quote'
+},{
+  quote: "You've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth.",
+  type: 'Popular quote'
+},{
+  quote: 'You only live once, but if you do it right, once is enough.',
+  type: 'Insipirational quote'
+},{
+  quote: 'Be the change that you wish to see in the world.',
+  type: 'Insipirational quote'
+},{
+  quote: "In three words I can sum up everthing I've learned about life: it goes on.",
+  type: 'Insipirational quote'
+},{
+  quote: "Don't walk in front of me... I may not follow; Don't walk behind me... I may not lead; Walk beside me... just be my friend.",
+  type: 'Insipirational quote'
+},{
+  quote: "If you tell the truth you don't have to remember anything.",
+  type: 'Insipirational quote'
+},{
+  quote: "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+  type: 'Insipirational quote'
+},{
+  quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+  type: 'Insipirational quote'
+},{
+  quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+  type: 'Insipirational quote'
+},{
+  quote: "We accept the love we think we deserve.",
+  type: 'Insipirational quote'
+},{
+  quote: "Without music, life would be a mistake.",
+  type: 'Insipirational quote'
+},{
+  quote: "There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everthing is a miracle.",
+  type: 'Insipirational quote'
+},{
+  quote: "We are all in gutter but some of us looking at the stars.",
+  type: 'Insipirational quote'
+},{
+  quote: "Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.",
+  type: 'Insipirational quote'
+},{
+  quote: "I've not failled. I've just found 10,000 ways that won't work.",
+  type: 'Insipirational quote'
+},{
+  quote: "I must not fear. Fear is the mind killer.",
+  type: 'Motivational quote'
+},{
+  quote: "Attitude is a choice. Happiness is a choice. Kindness is a choice. Giving is a choice. Whatever choice you make makes you. Choose wisely.",
+  type: 'Motivational quote'
+},{
+  quote: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+  type: 'Motivational quote'
+},{
+  quote: "Believe in yourself. You are braver than you thing, more talented than you know, and more capable of more than you imagine.",
+  type: 'Motivational quote'
+},{
+  quote: "Live the life of Your dreams: Be brave enough to live the life of your dreams according to your vision and purpose instead of the expectations and opinions of others.",
+  type: 'Motivational quote'
+},{
+  quote: "It's only after you've stepped outside your comfort zone that you begin to change, grow and transform.",
+  type: 'Motivational quote'
+},{
+  quote: "Do what is right not what is easy or what is popular.",
+  type: 'Motivational quote'
+},{
+  quote: "In three words I can sum up everthing I've learned about life: it goes on.",
+  type: 'Motivational quote'
+},{
+  quote: "Success is not how high you have climbed, but how you make positive difference to the world.",
+  type: 'Motivational quote'
+},{
+  quote: "Pursue what catches your heart, not what catches your eyes.",
+  type: 'Motivational quote'
+},{
+  quote: "Start each day with a positive thought and a grateful heart.",
+  type: 'Motivational quote'
+},{
+  quote: "Life is about accepting the challenges along the way; choosing to keep moving forward, and savoring the journey.",
+  type: 'Motivational quote'
+},{
+  quote: "Be grateful for what you already have while you pursue your goals. If you aren't grateful for what you already have, what makes you think you would be happy with more.",
+  type: 'Motivational quote'
+},{
+  quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  type: 'Success quote'
+},{
+  quote: "I can't give a sure-fire formula for success, but I can give you a formula for failure: try to please everybody all the time.",
+  type: 'Success quote'
+},{
+  quote: "Try not to become a man of success. Rather become a man of value.",
+  type: 'Success quote'
+},{
+  quote: "It's better to fall in originality than to succeed in imitation.",
+  type: 'Success quote'
+},{
+  quote: "When a new day begin dare to smile grateful.",
+  type: 'Success quote'
+},{
+  quote: "Letting go means to come to the realization that some people are part of your history, but not a part of your destiny.",
+  type: 'Success quote'
+},{
+  quote: "Success is getting what you want, happiness is wanting what you get.",
+  type: 'Success quote'
+},{
+  quote: "Failure is the condiment that gives success its flavour.",
+  type: 'Success quote'
+},{
+  quote: "Have no fear of perfection - you'll never reach it.",
+  type: 'Success quote'
+},{
+  quote: "Instead of worrying about what you cannot control, shift your energy to what you can create.",
+  type: 'Life quote'
+},{
+  quote: "Be the reason someone smiles. Be the reason someone feels loved and believes in the goodness in people.",
+  type: 'Life quote'
+},{
+  quote: "Accept yourself, love yourself, and keep moving forward. If you wnat to fly, you have to give up what weighs you down.",
+  type: 'Life quote'
+},{
+  quote: "Be mindful. Be grateful. Be positive. Be true. Be kind.",
+  type: 'Life quote'
+},{
+  quote: "Don't waste your time in anger, regrets, worries, and grudges. Life is too short to be unhappy.",
+  type: 'Life quote'
+},{
+  quote: "You cannot control the behaviour of others, but you can always choose how you respone to it.",
+  type: 'Life quote'
+}];
